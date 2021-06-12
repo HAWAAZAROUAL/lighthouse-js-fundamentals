a83ecb824ec6572f8ec86cbde88f98177adc5a8e
Hawaa <hawaazaroual@gmail.com>
2021-06-11 20:19:53 -0600

    Using conditionals to make wardrobe decisions

---
+++ conditionals.js
@@ -0,0 +1,28 @@
+//const raining= true;
+//const cold= false;
+
+/*
+if(raining){
+    console.log("Don't forget your umbrella!");
+}
+
+if(cold){
+    console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!"); 
+
+if(cold){
+    console.log("make sure to pick out a scarf!");
+} else { console.log("Short sleeves are fine.")
+}
+*/
+
+const temperature= -1;
+if(temperature<0){
+    console.log("make sure you wear a scarf.");
+} else if (temperature < 15){
+    console.log("short sleeves won't do.");
+} else { console.log("short sleeves are okay.");
+}
+console.log("Now you are ready to go outside.");
