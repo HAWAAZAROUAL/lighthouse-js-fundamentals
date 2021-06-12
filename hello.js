b94fccf6574b837c888689577ab204664e885af5
Hawaa <hawaazaroual@gmail.com>
2021-06-12 09:31:20 -0600

    added more examples to hello.js

--- hello.js
+++ hello.js
@@ -8,3 +8,15 @@ const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 sayHello("liz");
+
+//console.log
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+//return
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
