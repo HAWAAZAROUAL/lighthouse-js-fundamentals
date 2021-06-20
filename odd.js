59c2bb3111cbc8920844df2d46d596174a485cc2
Hawaa <hawaazaroual@gmail.com>
2021-06-20 14:13:23 -0600

    My Odd checker assignment

---
+++ odd.js
@@ -0,0 +1,21 @@
+function isOdd(num){
+  if(num % 2 === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log("3 is odd "+ isOdd(3))
+console.log("8 is odd " + isOdd(8))
+
+
+
+
+/* IGNORE THIS- SCRAP WORK- INCORRECT
+const isOdd= function (num){
+  if(num % 2 === 0){
+    return true
+  } else if (!num % === 0){
+    return false}
+}
+console.log("8 is odd: " + isOdd(8));
